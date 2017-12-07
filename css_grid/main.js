00743fba2fb9f0a6a91f110c76e1b55e855c9d1d
Patrick Seiuli <patrick.seiuli@gmail.com>
2017-12-07 16:17:43 -0700

    renamed images

--- css_grid/main.js
+++ css_grid/main.js
@@ -2,6 +2,20 @@ var pinkBox = document.getElementById("pinkbox")
 var pinkBox2 = document.getElementById("pinkbox2")
 var pinkBox3 =  document.getElementById("pinkbox3")
 
+var blueMouse = pinkBox.onmouseover
+var blueMouseOut = pinkBox.onmouseleave
+
+function mouseOver() {
+    if ( blueMouse === true )
+        blueMouse.style.backgroundColor = 'red'
+    else
+        mouseOut()
+}
+
+function mouseOut() {
+    blueMouseOut.style.backgroundColor = 'blue'
+}
+
 pinkBox.onmouseover = function() {
     pinkBox.style.backgroundColor = 'blue'
 }
@@ -11,5 +25,5 @@ pinkBox2.onclick = function() {
 }
 
 pinkBox3.onmouseenter = function() {
-    pinkBox3.style.backgroundImage = "url('smiling-face.png')"
+    pinkBox3.style.backgroundColor = 'yellow'
 }
